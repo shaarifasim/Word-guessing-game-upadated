ed5d89bca1c174fe7f5be5f2da3e9ea3cc68c1a7
Shaarif Asim <84714602+shaarifasim@users.noreply.github.com>
2022-07-30 17:23:19 +0500

    Add files via upload

---
+++ wordguessing game/wordguessing game/README.md
@@ -0,0 +1,1 @@
+"# UI-of-word-guessing-game" 
---
+++ wordguessing game/wordguessing game/app.js
@@ -0,0 +1,185 @@
+
+
+function forA() {
+
+    document.getElementById("inp").value += document.getElementsByClassName("buttonA")[0].innerText;
+
+
+}
+
+function forB() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[1].innerText;
+
+
+}
+
+function forC() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[2].innerText;
+
+
+}
+
+function forD() {
+
+    document.getElementById("inp").value += document.getElementsByClassName("buttonA")[3].innerText;
+
+
+}
+
+function forE() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[4].innerText;
+
+
+}
+
+function forF() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[5].innerText;
+
+
+}
+
+function forG() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[6].innerText;
+
+
+}
+
+function forH() {
+
+    document.getElementById("inp").value += document.getElementsByClassName("buttonA")[7].innerText;
+
+
+}
+
+function forI() {
+
+    document.getElementById("inp").value += document.getElementsByClassName("buttonA")[8].innerText;
+
+
+}
+
+function forJ() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[9].innerText;
+
+
+}
+
+function forK() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[10].innerText;
+
+
+}
+
+function forL() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[11].innerText;
+
+
+}
+
+function forM() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[12].innerText;
+
+
+}
+
+function forN() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[13].innerText;
+
+
+}
+
+function forO() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[14].innerText;
+
+
+}
+
+function forP() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[15].innerText;
+
+
+}
+
+function forQ() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[16].innerText;
+
+
+}
+
+function forR() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[17].innerText;
+
+
+}
+
+function forS() {
+
+    document.getElementById("inp").value += document.getElementsByClassName("buttonA")[18].innerText;
+
+
+}
+
+function forT() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[19].innerText;
+
+
+}
+
+function forU() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[20].innerText;
+
+
+}
+
+function forV() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[21].innerText;
+
+
+}
+
+function forW() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[22].innerText;
+
+
+}
+
+function forX() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[23].innerText;
+
+
+}
+
+function forY() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[24].innerText;
+
+
+}
+
+function forZ() {
+
+     document.getElementById("inp").value += document.getElementsByClassName("buttonA")[25].innerText;
+}
+function del(){
+     var clear = document.getElementsById("inp");
+     clear.innerText = ' ';
+}
